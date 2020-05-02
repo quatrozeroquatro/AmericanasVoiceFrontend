d706c1b4dfa97342e4a2ac3ab4f6d9af722a05c9
Julio Cesar da Silva Pereira <julio@iMac-de-Julio.local>
2020-05-02 19:32:40 -0300

    custom home

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+      <Image style={{width: "90%", resizeMode: "stretch"}} source={require("./assets/home-banner1.png")} />
+      <Image style={{width: "90%", resizeMode: "stretch"}} source={require("./assets/home-banner2.png")} />
+      <Image  style={{width: "100%", height: 280, resizeMode: "stretch"}} source={require("./assets/home-banner-slider.png")} />
+      <Image style={{width: "90%", resizeMode: "stretch"}} source={require("./assets/home-banner-iphone.png")} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+const ToolbarActions = (props) => (<View style={styles.toolbarActions}>
+  <Image source={require("./assets/busca-icon.png")} />
+  <Image source={require("./assets/cesta-compra-icon.png")} />
+</View>);
+
+function App() {
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+            headerStyle: styles.toolbar,
+            headerLeft: (props) => <Image style={styles.toolbarMenuHamburger} source={require("./assets/menu-hamburger-icon.png")} />,
+            headerTitle: (props) => <Image source={require("./assets/logo.png")} />,
+            headerRight: (props) => <ToolbarActions {...props} />
+          }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safearea: {
+    flex:1,
+    backgroundColor: "#EAEAEA"
+  },
+  toolbar: {
+    backgroundColor: "#E60014",
+  },
+  toolbarMenuHamburger: {
+    marginLeft: 20
+  },
+  toolbarActions: {
+    marginRight: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    alignItems: "center"
+    // backgroundColor: "pink"
+  }
+});
+
+export default App;
